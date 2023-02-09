34b65bc803b99876e1fd2d4fc1bb6abb4fddb3dd
natfletch <nef111605@yahoo.com>
2023-02-08 21:06:33 -0600

    started work on protrusion conversion and added W debug tab (will do more laater)

--- src/main/java/frc/robot/shuffleboard/ShuffleboardManager.java
+++ src/main/java/frc/robot/shuffleboard/ShuffleboardManager.java
@@ -3,30 +3,57 @@ package frc.robot.shuffleboard;
 import java.util.Arrays;
 import java.util.List;
 
+import edu.wpi.first.wpilibj.RobotState;
+import frc.robot.RobotContainer;
 import frc.robot.shuffleboard.tabs.DebugTab;
 import frc.robot.shuffleboard.tabs.DrivetrainTab;
 import frc.robot.shuffleboard.tabs.IntakeTab;
 import frc.robot.shuffleboard.tabs.MainCompetitionTab;
 import frc.robot.shuffleboard.tabs.ProtruderTab;
 import frc.robot.shuffleboard.tabs.VisionTab;
+import frc.robot.util.SWATXboxController;
 
 public class ShuffleboardManager {
-
     private List<ShuffleboardTabBase> mShuffleboardTabs;
+    private List<ShuffleboardTabBase> mDebugTabs;
+    private boolean testAdded;
+    private static SWATXboxController mController;
 
     public ShuffleboardManager(){
-        mShuffleboardTabs = Arrays.asList(new MainCompetitionTab(), new DebugTab(), new DrivetrainTab(), new IntakeTab(), new ProtruderTab(), new VisionTab());
+        mShuffleboardTabs = Arrays.asList(new MainCompetitionTab(), new DebugTab());
+        mDebugTabs = Arrays.asList(new DrivetrainTab(), new IntakeTab(), new ProtruderTab(), new VisionTab());
+        testAdded = false;
+        mController = RobotContainer.S_DRIVECONTROLS.getDriverController();
     }
 
     public void registerTabs(){
         for (ShuffleboardTabBase tab : mShuffleboardTabs) {
             tab.createEntries();
         }
+
+        if (mController.getStartButton() && mController.getBackButton() || testAdded){
+            testAdded=true;
+            for (ShuffleboardTabBase dTab : mDebugTabs){
+                dTab.createEntries();
+            }
+        }
     }
 
     public void updateAllTabs(){
         for (ShuffleboardTabBase tab : mShuffleboardTabs) {
             tab.update();
+        }
+
+        if (mController.getStartButton() && mController.getBackButton() || testAdded){
+            if(testAdded==false){
+                testAdded=true;
+                for (ShuffleboardTabBase dTab : mDebugTabs){
+                    dTab.createEntries();
+                }
+            }
+            for (ShuffleboardTabBase dTab : mDebugTabs){
+                dTab.update();
+            }
         }
     }
 }
